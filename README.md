89f2e281405b4d2438cbb93424e2400d73a19787
lmdlalana023 <lmdlalana023@student.wethinkcode.co.za>
2024-10-03 13:24:20 +0200

    Revamp the code

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Calcutor project. Challenging myself ny doing JavaScript challenges
 
 
 ### What I learned
-- eval()
+- eval() & why you shouldn't use it in production
 - onclick attribute on html button 
 
 ### thoughts 
 - After learning about eval the calculator was not as hard as I initially thought
 although i had to google a lot of things.
+- How to use regular expressions in javascript
+- The use of the reduce() function in javascript
--- calc.js
+++ calc.js
@@ -1,18 +1,46 @@
-const inputLabel = document.getElementById('inputLabel');
+const inputLabel = document.getElementById("inputLabel");
 
-function pushBtn(item){
-    let pushed = item.innerText;
-    if(pushed == '='){
-        //calculate
-        inputLabel.innerHTML = eval(inputLabel.innerText);
-    }else if(pushed == 'AC'){
-        //clear
-        inputLabel.innerHTML = '0';
-    }else{
-        if(inputLabel.innerHTML == '0'){
-            inputLabel.innerText = pushed;
-        }else{
-            inputLabel.innerText += pushed;
-        }
+function pushBtn(item) {
+  let pushed = item.innerText;
+
+  if (pushed == "=") {
+    // Calculate the result using a safe approach
+    try {
+      inputLabel.innerText = calculate(inputLabel.innerText);
+    } catch {
+      inputLabel.innerText = "Error";
     }
+  } else if (pushed == "AC") {
+    // Clear
+    inputLabel.innerText = "0";
+  } else {
+    // Update input
+    if (inputLabel.innerText == "0") {
+      inputLabel.innerText = pushed;
+    } else {
+      inputLabel.innerText += pushed;
+    }
+  }
+}
+
+function calculate(expression) {
+  const tokens = expression.match(/(\d+|\+|\-|\*|\/)/g);
+  if (!tokens) throw new Error("Invalid expression");
+
+  let stack = [];
+  let currentOp = "+";
+
+  for (let token of tokens) {
+    if (/\d/.test(token)) {
+      let num = parseFloat(token);
+      if (currentOp == "+") stack.push(num);
+      else if (currentOp == "-") stack.push(-num);
+      else if (currentOp == "*") stack.push(stack.pop() * num);
+      else if (currentOp == "/") stack.push(stack.pop() / num);
+    } else {
+      currentOp = token;
+    }
+  }
+
+  return stack.reduce((acc, num) => acc + num, 0);
 }
